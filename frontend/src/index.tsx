7ebb6982d9f88e248c3d32e5373be1a3aed8ed21
SricharithaKatta <sricharithakatta09@gmail.com>
2026-03-18 10:30:00 -0500

    Updated some files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,21 @@
+import jsonfile from "jsonfile";
+import moment from "moment";  
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+ const path = "./data.json";
+ const date = moment().format();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+ 
+const data = {
+  date : date, 
+}
 
+jsonfile.writeFile(path,data);
 root.render(
   <React.StrictMode>
     <App />
